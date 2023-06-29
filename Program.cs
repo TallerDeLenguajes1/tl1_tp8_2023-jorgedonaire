7ee6cf5d11d99be87c2b2ea9c75034a23b077f98
Jorge Donaire <jorgedonaire93@gmail.com>
2023-06-28 23:18:55 -0300

    Listado de archivos en consola finalizado

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
 ﻿class Program
 {
     private static void Main(){
-        
+        string rutaCarpeta;
+        do
+        {
+            Console.WriteLine("Ingrese la ruta de la carpeta");
+            rutaCarpeta = @Console.ReadLine();
+            //@"D:\Jorge\Facultad\Programacion\TallerdeLenguaje1"
+        } while (string.IsNullOrEmpty(rutaCarpeta));
               
-            
+        if (Directory.Exists(rutaCarpeta))
+        {
+            Console.WriteLine("**** Listado de archivos existentes en la carpeta *****");
+            var archivosCarpeta = Directory.GetFiles(rutaCarpeta);
+
+            foreach (var archivo in archivosCarpeta)
+            {
+                Console.WriteLine(archivo);
+            }
+
+            string rutaArchivo = "index.csv";
+        }else
+        {
+            Console.WriteLine("Ruta incorrecta o carpeta inexistente");
+        } 
         
     }
 }
